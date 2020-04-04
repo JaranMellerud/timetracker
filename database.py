2ef837da811dc172439ef1d061c7785741ea33e0
Jaran Mellerud <j.mellerud@gmail.com>
2020-04-04 18:34:22 +0200

    commit

--- database.py
+++ database.py
@@ -1,23 +1,32 @@
 import sqlite3
 
-# database connection and cursor in sqlite
-conn = sqlite3.connect("timetracker.db")
-cur = conn.cursor()
 
-# creating table in database if table not exists already
-cur.execute("CREATE TABLE IF NOT EXISTS Activities (id INTEGER PRIMARY KEY AUTOINCREMENT, activity TEXT, time_total REAL DEFAULT '0', time_today REAL DEFAULT 0)")
-conn.commit()
+class DataBase():
+    def __init__(self):
+        self.conn = sqlite3.connect("timetracker.db")
+        self.cur = self.conn.cursor()
 
-# function to check how many activities added
-def numActivities():
-    num_activities = len(cur.execute("SELECT * FROM Activities").fetchall())
-    return num_activities
+    # creating table in database if table not exists already
+    def createTable(self):
+        self.cur.execute("CREATE TABLE IF NOT EXISTS Activities (id INTEGER PRIMARY KEY AUTOINCREMENT, activity TEXT, time_total REAL DEFAULT '0', time_today REAL DEFAULT 0)")
+        self.conn.commit()
 
-# function to get all the activities with time
-def getActivities():
-    activities = cur.execute("SELECT activity, time_total, time_today FROM Activities").fetchall()
-    return activities
+    # function to check how many activities there are in the database
+    def numActivities(self):
+        self.num_activities = len(self.cur.execute("SELECT * FROM Activities").fetchall())
+        return self.num_activities
 
+    # function to get all the activities with time spent in total and time spent today
+    def getActivities(self):
+        self.activities = self.cur.execute("SELECT activity, time_total, time_today FROM Activities").fetchall()
+        return self.activities
 
-#cur.execute("delete from Activities")
-#conn.commit()
+    # function to delete all activities
+    def deleteAllFromTable(self):
+        self.cur.execute("DELETE FROM Activities")
+        self.conn.commit()
+
+    # function to add activity to database
+    def addActivity(self, activity_to_add):
+        self.cur.execute(f'INSERT INTO Activities (activity) VALUES ("{activity_to_add.get()}")')
+        self.conn.commit()
--- timetracker.py
+++ timetracker.py
@@ -1,89 +1,71 @@
 import tkinter as tk
-import time
-from functools import partial
-from database import conn, cur, numActivities, getActivities
+from database import DataBase
 
 
 class TimeTracker:
-    def __init__(self, root):
-        # setting default values for height, width and font
-        self.root = root
-        self.root.title("Time Tracker")
-        num_activities = numActivities()
-        HEIGHT = 75 + num_activities * 35
+    def __init__(self, master):
+        # loading variables to set properties
+        self.num_activities = database.numActivities()
+        self.activities = database.getActivities()
+
+        # properties for sizes and fonts
+        HEIGHT = 75 + self.num_activities * 35
         WIDTH = 300
+        TITLE = "Time Tracker"
         FONT_TYPE = 'helvetica'
         BIG_LABEL_FONT = (FONT_TYPE, '16', "bold")
         SMALL_LABEL_FONT = (FONT_TYPE, '13')
         ENTRY_FONT = (FONT_TYPE, '12')
-        
-        self.canvas = tk.Canvas(self.root, width=WIDTH, height=HEIGHT)
+
+        self.master = master
+        self.master.title(TITLE)
+        self.canvas = tk.Canvas(self.master, width=WIDTH, height=HEIGHT)
         self.canvas.pack()
-
-        # if user doesnt have activities yet show label_add. else show the activities with time spent in a grid
-        if num_activities == 0:
-            self.label_add = tk.Label(self.canvas, text="Add An Activity To Start!")
-            self.label_add.config(font=BIG_LABEL_FONT)
+        
+        # showing different interface if no activities added
+        if self.num_activities == 0:
+            self.label_add = tk.Label(self.canvas, text="Add An Activity To Start!", font=BIG_LABEL_FONT)        
             self.canvas.create_window(140, 15, window=self.label_add)
-
-            # section to add new activities
+            # entry to add new activities
             self.entry_add = tk.Entry(self.canvas, text="Activity", font=ENTRY_FONT)
-            self.canvas.create_window(110, 50, window=self.entry_add)   
-            self.button_add = tk.Button(text="Add", command=self.addToDatabase)
+            self.canvas.create_window(110, 50, window=self.entry_add)
+            self.button_add = tk.Button(text="Add", command=lambda:[database.addActivity(self.entry_add), self.entry_add.delete(first=0, last="end")])
+            self.button_add.focus_set()
             self.canvas.create_window(250, 50, window=self.button_add)
         else:
-            self.frame_show = tk.Frame(self.canvas)
-            self.frame_show.place(relwidth=1, relheight=1)
-            self.label_activity = tk.Label(self.frame_show, text="Activity")
-            self.label_activity.config(font=SMALL_LABEL_FONT)
-            self.label_activity.grid(row=0, column=0)
-            
-            self.label_total = tk.Label(self.frame_show, text="Total")
-            self.label_total.config(font=SMALL_LABEL_FONT)
+            # grid with activities, total and time in a frame
+            self.frame = tk.Frame(self.canvas)
+            self.frame.place(relwidth=1, relheight=1)
+            self.label_activity = tk.Label(self.frame, text="Activity", font=SMALL_LABEL_FONT)
+            self.label_activity.grid(row=0, column=0)   
+            self.label_total = tk.Label(self.frame, text="Total", font=SMALL_LABEL_FONT)
             self.label_total.grid(row=0, column=1)
-
-            self.label_time = tk.Label(self.frame_show, text="Time")
-            self.label_time.config(font=SMALL_LABEL_FONT)
+            self.label_time = tk.Label(self.frame, text="Time", font=SMALL_LABEL_FONT)
             self.label_time.grid(row=0, column=2)
 
             # creating rows with all the activities
-            activities = getActivities()
-            for ind, activity in enumerate(activities):
-                for index, value in enumerate(activity):
-                    self.label_activity = tk.Label(self.frame_show, text=value)
-                    self.label_activity.grid(row=ind+1, column=index)     
-                self.button_start_stop = tk.Button(self.frame_show, text="Start/Stop", command=partial(self.updateClock, row=ind+1, column=2, time_click=time.time()))
-                self.button_start_stop.grid(row=ind+1, column=4)
+            self.createActivityRows()
 
             # section to add new activities
             self.entry_add = tk.Entry(self.canvas, text="Activity", font=ENTRY_FONT)
-            self.canvas.create_window(110, 45 + num_activities * 35, window=self.entry_add)
-            self.button_add = tk.Button(text="Add", command=self.addToDatabase)
-            self.canvas.create_window(250, 45 + num_activities * 35, window=self.button_add)
+            self.canvas.create_window(110, 45 + self.num_activities * 35, window=self.entry_add)
+            self.button_add = tk.Button(text="Add", command=lambda:[database.addActivity(self.entry_add), self.entry_add.delete(first=0, last="end"), self.createActivityRows()])
+            self.canvas.create_window(250, 45 + self.num_activities * 35, window=self.button_add)
 
-    # function to add activity to activities table in timetracker.db
-    def addToDatabase(self):
-        cur.execute(f'INSERT INTO Activities (activity) VALUES ("{self.entry_add.get()}")')
-        conn.commit()
-
-    # function that runs the clock and displays time. the function is initialized by the start_stop_clock function
-    def updateClock(self, row, column, time_click):
-        now = time.time()
-        timer = self.frame_show.grid_slaves(row=row, column=column)[-1]
-        time_passed = now - time_click
-        timer.config(text=time_passed)
-        timer.after(1000, self.updateClock, row, column, time_click)
-
-    # starts and stops the clock. command for the start/stop button
-    #def startClock(self):
-        #time_click = 
-            
-        
-        
-    
+    # function to create rows with activities and time
+    def createActivityRows(self):
+        self.activities = database.getActivities()
+        for ind, activity in enumerate(self.activities):
+            for index, value in enumerate(activity):
+                self.label_activity = tk.Label(self.frame, text=value)
+                self.label_activity.grid(row=ind+1, column=index)     
+            self.button_start_stop = tk.Button(self.frame, text="Start/Stop")
+            self.button_start_stop.grid(row=ind+1, column=4)
 
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    timetracker = TimeTracker(root)
-    root.mainloop()
+    database = DataBase()
+    database.createTable()
+    master = tk.Tk()
+    tt = TimeTracker(master)
+    master.mainloop()
